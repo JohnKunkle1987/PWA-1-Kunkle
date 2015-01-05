9ebaecb24cdeb36f460f4e1a557a54b1056f7d5c
JohnKunkle1987 <Myvyrus@fullsail.edu>
2015-01-05 12:36:23 -0500

    asdf

---
+++ PWA1-GitHubClone-master/PWA1-January2015/PWA1-Initial_PreTest/PreTest_Layout_Assets/css/kunkle_js.js
@@ -0,0 +1,18 @@
+alert("Welcome to my online store");
+console.log("test");
+
+var cost = prompt("Enter the cost of an item");
+
+if (isNan(cost)||cost===){
+	console.log ("please enter a valid number");
+	}
+var discount = prompt("enter your discount for the item");
+
+if (isNan(discount||discount===){
+	console.log ("enter a valid number");
+	}
+else if(discount>100||discount||<0){
+	console.log("enter a valid number");
+	}
+function(cost,discount){
+	
